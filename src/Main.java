aae030571521cf4af20ed59fc4a9abc4e1703b47
Александр Стрюков <sanyajump16@gmail.com>
2022-02-02 20:59:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1000;
+        int oneMile = 20;
+        int bonus = ticket / oneMile;
+        System.out.println("Количество начисленных мили за купленный билет: " + bonus);
+    }
+}
+
